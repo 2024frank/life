e40f47f0c7d727173c710fd301d1609c4236dcd8
Frank Kusi Appiah <2024frank@gmail.com>
2026-01-31 09:03:13 +1300

    Initial Commit

---
+++ knowbest/knowbestApp.swift
@@ -0,0 +1,17 @@
+//
+//  knowbestApp.swift
+//  knowbest
+//
+//  Created by Frank Kusi Appiah on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct knowbestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
